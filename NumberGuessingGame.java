8c1233fb7a28be9ef027731eebd25a108ef6f0a8
1806-aks <asingh2002.us@GMAIL.COM>
2025-02-11 14:46:35 +0530

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -5,11 +5,9 @@ public class NumberGuessingGame {
     private static final int MAX_ATTEMPTS = 10;
     private static int totalScore = 0;
     private static int roundsPlayed = 0;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String playAgain;
-
         do {
             playGame(scanner);
             System.out.print("Do you want to play again? (yes/no): ");
@@ -19,20 +17,15 @@ public class NumberGuessingGame {
         System.out.println("Thanks for playing! You played " + roundsPlayed + " rounds with a total score of " + totalScore + ".");
         scanner.close();
     }
-
     private static void playGame(Scanner scanner) {
         Random random = new Random();
         int numberToGuess = random.nextInt(100) + 1; 
         int attempts = 0;
-
         System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("I have selected a number between 1 and 100. You have " + MAX_ATTEMPTS + " attempts to guess it.");
-
         while (attempts < MAX_ATTEMPTS) {
             System.out.print("Enter your guess: ");
             int guess;
-
-        
             try {
                 guess = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
@@ -53,7 +46,6 @@ public class NumberGuessingGame {
                 return; 
             }
         }
-
         System.out.println("Sorry, you've used all " + MAX_ATTEMPTS + " attempts. The number was " + numberToGuess + ".");
         totalScore += 0; 
         roundsPlayed++;
